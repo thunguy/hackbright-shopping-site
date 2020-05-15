66ea2d3439898f12ea11c45d618aeebf245a270d
Thu Nguyen <thunguy@me.com>
2020-05-14 20:25:28 -0700

    create a {cart}  within {session} that records melons added to cart during active session

--- shoppingsite.py
+++ shoppingsite.py
@@ -8,7 +8,6 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 
 from flask import Flask, render_template, redirect, flash, session
 import jinja2
-
 import melons
 
 app = Flask(__name__)
@@ -92,18 +91,31 @@ def add_to_cart(melon_id):
     page and display a confirmation message: 'Melon successfully added to
     cart'."""
 
-    # TODO: Finish shopping cart functionality
+    # check if a "cart" exists in the session, and create one (an empty
+    # dictionary keyed to the string "cart") if not
+    cart = session.setdefault("cart", {})
+   
+        # # same as saying...
+        # if "cart" not in session:
+        #     cart = session["cart"]
+        # else:
+        #     cart = session["cart"] = {}
 
-    # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
-    # - check if the desired melon id is the cart, and if not, put it in
-    # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
+    # check if the desired melon_id is the cart, and if not, put it in
+    # and increment the count for that melon_id by 1
+    cart[melon_id] = cart.get(melon_id, 0) + 1
 
-    return "Oops! This needs to be implemented!"
+    added_melon = melons.get_by_id(melon_id)
+
+    # flash a user success message on next page load 
+    flash(f"{added_melon.common_name} has been successfully added to cart")
+
+    #print current session's cart to the terminal
+    print("session:", session)
+    print("cart:", cart)
+
+    # Redirect to shopping cart page
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
